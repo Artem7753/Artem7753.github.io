33c3425f80ed78904e9e01e52ffba228c9e1e500
artem <artem7753@mail.ru>
2018-04-09 16:37:30 +0300

    added stars and form

--- library/main.js
+++ library/main.js
@@ -1,9 +1,11 @@
 var addButton = document.getElementById("add_book");
 var addForm = document.getElementById("add_new_book_form");
-addButton.addEventListener("click",function(){
+addButton.addEventListener("click", function (){
     addForm.style.display = "block";
+    console.log("open");
 })
 var closeButton = document.getElementById("exit");
 closeButton.addEventListener("click",function(){
     addForm.style.display = "none";
+    console.log("closed");
 })
